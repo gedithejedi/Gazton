e3b0c4f344d2a5cfa7502bcd325338dacd924e28
gedithejedi <gediminaspetreikis@yahoo.com>
2023-11-04 16:24:56 +0000

    fix: convert the toasts to react-hot-toast

--- src/utils/useSendNotification.tsx
+++ src/utils/useSendNotification.tsx
@@ -1,12 +1,11 @@
-import { useToast } from "@chakra-ui/react";
 import { useCallback, useState } from "react";
 import { useW3iAccount } from "@web3inbox/widget-react";
 import { INotification } from "./types";
 import { sendNotification } from "./fetchNotify";
+import toast from "react-hot-toast";
 
 function useSendNotification() {
   const [isSending, setIsSending] = useState<boolean>(false);
-  const toast = useToast();
   const { account } = useW3iAccount();
 
   const handleSendNotification = useCallback(
@@ -21,22 +20,11 @@ function useSendNotification() {
           notification,
         });
         setIsSending(false);
-
-        toast({
-          status: success ? "success" : "error",
-          position: "top",
-          variant: "subtle",
-          colorScheme: success ? "purple" : "red",
-          title: success ? notification.title : "Message failed.",
-        });
+        toast[success ? 'success' : 'error'](success ? notification.title : "Message failed.");
       } catch (error: any) {
         setIsSending(false);
         console.error({ sendNotificationError: error });
-        toast({
-          status: "error",
-          title: error.message,
-          description: error.cause,
-        });
+        toast.error(error.message + " " + error.cause)
       }
     },
     [toast, account]
